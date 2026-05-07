2aa3bc3c5b37a0878c390249aa443287e402eaf8
2naain <jdlozano31@ucatolica.edu.co>
2026-05-07 14:36:20 -0500

    now should be starting fr

--- model.py
+++ model.py
@@ -10,20 +10,3 @@ class Dog(SQLModel, table = True):
         default_factory=datetime.utcnow(),
         sa_column_kwargs={"server_default": "NOW()"}
     )
-
-class Sticker(SQLModel, table = True):
-    __tablename__ = "Stickers"
-
-    created: datetime = Field(
-        default_factory=datetime.utcnow(),
-        sa_column_kwargs={"server_default": "NOW()"}
-    )
-
-
-class Book(SQLModel, table = True):
-    __tablename__ = "Books"
-
-    created: datetime = Field(
-        default_factory=datetime.utcnow(),
-        sa_column_kwargs={"server_default": "NOW()"}
-    )
